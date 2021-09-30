6e540cfa9a59a2a7d4fbe231632c6fd635c8c108
c_h_o <c_h_o@DESKTOP-RPURV9O>
2021-09-30 18:44:06 +0200

    Primer Commit.

---
+++ RomboGit/src/RomboGit/Rombo.java
@@ -0,0 +1,47 @@
+package RomboGit;
+
+public class Rombo {
+	 
+    public static void main(String[] args)
+    {
+        int max=30;
+        int i=0,j=0,k=0;
+ 
+      
+        if(max%2==0)
+        {
+            max++;
+        }
+ 
+      
+        for(i=1;i<=max;i=i+2)
+        {
+     
+            for(k=max+1;k>=i;k=k-2)
+            {
+                System.out.print(" ");
+            }
+      
+            for(j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+   
+        for(i=max;i>=1;i=i-2)
+        {
+         
+            for(k=i;k<=max+2;k=k+2)
+            {
+                System.out.print(" ");
+            }
+            
+            for(j=i-2;j>0;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
